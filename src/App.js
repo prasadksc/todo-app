1f16bfdc1725a259818b4fdf2e58e8aa46134fe2
prasadksc <prasadksc123@gmail.com>
2021-06-16 15:01:58 +0530

    hello

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import ListItems from './listItems';
+import './App.css';
+
+
+
+class App extends Component {
+  state = {
+    items: [],
+    currentitem: {
+      text: '',
+      key: ''
+    }
+  }
+
+  handleInput = e => {
+    this.setState({
+      currentitem: {
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+
+  addItem = e => {
+    e.preventDefault()
+    const newItem = this.state.currentitem
+    if (newItem.text !== '') {
+      const newitems = [...this.state.items, newItem]
+      this.setState({
+        items: newitems,
+        currentitem: {
+          text: '',
+          key: ''
+        }
+      })
+    }
+  }
+
+
+  setItemUpdate = (text, key) => {
+    const items = this.state.items
+    items.map(item => {
+      if (item.key === key) {
+        return item.text = text
+      }
+      this.setState({ items: items })
+    })
+
+  }
+
+
+  deleteItem = key => {
+    const filteredItems = this.state.items.filter(item => item.key !== key)
+    this.setState({ items: filteredItems })
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>What's a Plan for Today</h1>
+          <form className="todo-form" onSubmit={this.addItem}>
+            <input type="text" onChange={this.handleInput} value={this.state.currentitem.text} placeholder="Add a Todo..." />
+            <button type="submit">Add</button>
+          </form>
+        </header>
+        <ListItems items={this.state.items} deleteItem={this.deleteItem} setItemUpdate={this.setItemUpdate} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/listItems.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './listItems.css'
+import { RiCloseCircleLine } from 'react-icons/ri';
+
+
+class ListItems extends Component {
+    render() {
+        const { items, deleteItem, setItemUpdate } = this.props
+        return items.map(item => (
+            <div className="list" key={item.key}>
+                <p> <input type="text" id={item.key} onChange={(e) => setItemUpdate(e.target.value, item.key)} value={item.text} s />
+                    <span> <RiCloseCircleLine
+                        onClick={() => deleteItem(item.key)}
+                        className='delete-icon'
+                    /></span>
+                </p>
+            </div>
+        ))
+    }
+}
+
+
+
+
+
+export default ListItems;
